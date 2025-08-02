e433d6d86cfb64a048fd9aa656052e26108162f7
Nico Jackson <nicolajack@gmail.com>
2025-08-02 11:50:15 -0400

    solved problem 58 in python

---
+++ easy/58.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s.strip()
+        count = 0
+        i = len(s) - 1
+        while (s[i] != ' ' and i >= 0):
+            count += 1
+            i -= 1
+        return count
